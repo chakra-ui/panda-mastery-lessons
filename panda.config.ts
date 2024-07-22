aa7a1a340fddd015a34ad061914f1669ad544edf
Esther Agbaje <folasadeagbaje@gmail.com>
2024-07-22 18:49:26 +0100

    add mod4 lsn1

--- panda.config.ts
+++ panda.config.ts
@@ -14,39 +14,7 @@ export default defineConfig({
   theme: {
     extend: {},
   },
-  // Patterns here
-  patterns: {
-    extend: {
-      scrollable: {
-        properties: {
-          size: { type: 'token', value: 'sizes' },
-          thumbColor: { type: 'token', value: 'colors' },
-          trackColor: { type: 'token', value: 'colors' },
-        },
-        transform(properties) {
-          const { size, thumbColor, trackColor, ...rest } = properties;
 
-          return {
-            overflow: 'auto',
-            '&::-webkit-scrollbar': {
-              width: size,
-              height: size,
-            },
-            '&::-webkit-scrollbar:window-inactive': {
-              opacity: '0',
-            },
-            '&::-webkit-scrollbar-thumb': {
-              background: thumbColor,
-            },
-            '&::-webkit-scrollbar-track': {
-              background: trackColor,
-            },
-            ...rest,
-          };
-        },
-      },
-    },
-  },
   // The output directory for your css system
   outdir: 'styled-system',
   jsxFramework: 'react',
--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,16 @@
-import { css } from '../styled-system/css';
-import { scrollable } from '../styled-system/patterns';
-
-const itemClassName = css({
-  bg: 'white',
-  borderWidth: '1px',
-  borderColor: 'lightgray',
-  padding: '48px',
-  borderRadius: '10px',
-});
+import { center } from '../styled-system/patterns';
+import { ColorModeToggle } from './color-mode-toggle';
 
 function App() {
   return (
     <div
-      className={css({
+      className={center({
         minH: '100dvh',
         bg: '#F8FAFC',
-        pt: '96px',
+        color: 'black',
       })}
     >
-      <div
-        className={scrollable({
-          display: 'flex',
-          flexDir: 'column',
-          gap: '24px',
-          maxH: '240px',
-          px: '12px',
-          size: '8',
-          thumbColor: 'green.500',
-          trackColor: 'pink.700',
-        })}
-      >
-        <div className={itemClassName}>Box 1</div>
-        <div className={itemClassName}>Box 2</div>
-        <div className={itemClassName}>Box 3</div>
-        <div className={itemClassName}>Box 4</div>
-      </div>
+      <ColorModeToggle />
     </div>
   );
 }
