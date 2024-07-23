636c1455ddc82500507a7d674c0d75e852c82819
Esther Agbaje <folasadeagbaje@gmail.com>
2024-07-23 13:59:10 +0100

    add mod9 lsn4

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,42 @@
 import { cva } from '../styled-system/css';
 import { styled } from '../styled-system/jsx';
 
-const toolbarRecipe = cva({
+const statusLabelRecipe = cva({
   base: {
-    position: 'absolute',
-    top: 'sm',
-    right: 'twoXS',
-    display: 'flex',
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderColor: 'borderBold',
+    marginLeft: 'lg',
+    whiteSpace: 'nowrap',
+    maxWidth: '20rem',
+    fontSize: 'sm',
+    transition: `opacity 200ms ease-out 0s,
+  clip 200ms ease-out 0s, min-width 200ms ease-out 0s,
+  max-width 200ms ease-out 0s, padding 200ms ease-out 0s`,
+    '--gradient-end': 'colors.accentBg',
+    backgroundImage: `linear-gradient(90deg, var(--gradient-end), #fffd80)`,
+  },
+
+  variants: {
+    isMinimized: {
+      true: {
+        opacity: 0,
+        padding: 'none',
+        margin: 'none',
+        maxWidth: 0,
+        minWidth: 0,
+        border: 0,
+      },
+    },
+    isPrompt: {
+      true: {
+        color: 'accentText',
+        textTransform: 'none',
+        borderRadius: 'md',
+      },
+      false: {
+        color: 'mutedText',
+        textTransform: 'uppercase',
+      },
+    },
   },
 });
 
-export const StyledHeaderToolbar = styled('div', toolbarRecipe);
+export const StyledStatusLabel = styled('div', statusLabelRecipe);
