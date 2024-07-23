a56cc208c3183e969f8d9b6fcb4f0d66970f2ca1
Esther Agbaje <folasadeagbaje@gmail.com>
2024-07-23 14:48:53 +0100

    add mod11 lsn4

--- src/App-atomic-recipe.tsx
+++ src/App-atomic-recipe.tsx
@@ -1,71 +1,123 @@
-import { css, cva } from '../styled-system/css';
-import { hstack } from '../styled-system/patterns';
-import { styled } from '../styled-system/jsx';
+import { RecipeVariantProps, css, sva } from '../styled-system/css';
+import { HStack, Stack } from '../styled-system/jsx';
+import { JsxStyleProps } from '../styled-system/types';
 
-export const badgeRecipe = cva({
+const alertRecipe = sva({
+  slots: ['root', 'icon', 'title', 'description'],
   base: {
-    color: 'white',
-    fontSize: 'xl',
-    fontWeight: 'bold',
-    textTransform: 'uppercase',
-    borderRadius: 'sm',
-    lineHeight: '1.33',
-    px: '2',
+    root: {
+      px: '16px',
+      py: '12px',
+    },
+    icon: {
+      width: '24px',
+      height: '24px',
+    },
+    title: {
+      fontWeight: 'bold',
+      fontSize: '16px',
+    },
+    description: {
+      fontSize: '16px',
+    },
   },
   variants: {
     status: {
-      neutral: {
-        colorPalette: 'gray',
-      },
       info: {
-        colorPalette: 'blue',
-      },
-      success: {
-        colorPalette: 'green',
+        root: {
+          colorPalette: 'blue',
+        },
       },
       error: {
-        colorPalette: 'red',
+        root: {
+          colorPalette: 'red',
+        },
+      },
+      warning: {
+        root: {
+          colorPalette: 'orange',
+        },
+      },
+      success: {
+        root: {
+          colorPalette: 'green',
+        },
       },
     },
     kind: {
-      solid: {
-        bg: 'colorPalette.500',
+      subtle: {
+        root: {
+          bg: 'colorPalette.100',
+        },
+        icon: {
+          color: 'colorPalette.600',
+        },
       },
-      outline: {
-        borderWidth: '0.5',
-        borderColor: 'colorPalette.400',
-        color: 'colorPalette.500',
+      solid: {
+        root: {
+          bg: 'colorPalette.600',
+          color: 'white',
+        },
       },
     },
   },
   defaultVariants: {
     status: 'info',
-    kind: 'outline',
+    kind: 'subtle',
   },
 });
 
-const Badge = styled('div', badgeRecipe);
+function InfoIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path d='M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM11 11V17H13V11H11ZM11 7V9H13V7H11Z' />
+    </svg>
+  );
+}
+
+type AlertVariantProps = RecipeVariantProps<typeof alertRecipe>;
+
+type AlertProps = AlertVariantProps &
+  JsxStyleProps & {
+    title: string;
+    description: string;
+  };
+
+function Alert(props: AlertProps) {
+  const [recipeProps, restProps] = alertRecipe.splitVariantProps(props);
+  const styles = alertRecipe.raw(recipeProps);
+  const { title, description, ...otherProps } = restProps;
+  return (
+    <div className={css(styles.root, otherProps)}>
+      <HStack gap='3'>
+        <InfoIcon className={css(styles.icon)} />
+        <Stack gap='0'>
+          <div className={css(styles.title)}>{title}</div>
+          <div className={css(styles.description)}>{description}</div>
+        </Stack>
+      </HStack>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div
-      className={css({
-        minH: '100dvh',
-        pt: '24',
-        px: '6',
-      })}
-    >
-      <div className={hstack({ gap: '8', maxW: '490px', mx: 'auto' })}>
-        <Badge bg='orange.500' status='neutral' kind='solid'>
-          Badge
-        </Badge>
-        <Badge status='info' kind='solid'>
-          Badge
-        </Badge>
-        <Badge status='success'>Badge</Badge>
-        <Badge status='error'>Badge</Badge>
-      </div>
-    </div>
+    <Stack minH='100dvh' pt='14' px='6'>
+      <Alert
+        bg='orange'
+        status='info'
+        kind='subtle'
+        title='Title'
+        description='This is a description'
+      />
+    </Stack>
   );
 }
 
--- src/App-config-recipe.tsx
+++ src/App-config-recipe.tsx
@@ -1,30 +1,55 @@
-import { css } from '../styled-system/css';
-import { styled } from '../styled-system/jsx';
-import { hstack } from '../styled-system/patterns';
-import { badge } from '../styled-system/recipes';
+import { Box, HStack, Stack } from '../styled-system/jsx';
+import { AlertVariantProps, alert } from '../styled-system/recipes';
+import { JsxStyleProps } from '../styled-system/types';
 
-const Badge = styled('div', badge);
+interface AlertProps extends AlertVariantProps, JsxStyleProps {
+  title: string;
+  description: string;
+}
+
+function InfoIcon(props: React.ComponentProps<'svg'>) {
+  return (
+    <svg
+      width='24'
+      height='24'
+      viewBox='0 0 24 24'
+      fill='currentColor'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path d='M12 22C6.477 22 2 17.523 2 12C2 6.477 6.477 2 12 2C17.523 2 22 6.477 22 12C22 17.523 17.523 22 12 22ZM11 11V17H13V11H11ZM11 7V9H13V7H11Z' />
+    </svg>
+  );
+}
+
+function Alert(props: AlertProps) {
+  const [recipeProps, restProps] = alert.splitVariantProps(props);
+  const classes = alert(recipeProps);
+  const { title, description, ...otherProps } = restProps;
+  return (
+    <Box className={classes.root} {...otherProps}>
+      <HStack gap='3'>
+        <InfoIcon className={classes.icon} />
+        <Stack gap='0'>
+          <div className={classes.title}>{title}</div>
+          <div className={classes.description}>{description}</div>
+        </Stack>
+      </HStack>
+    </Box>
+  );
+}
 
 function App() {
   return (
-    <div
-      className={css({
-        minH: '100dvh',
-        pt: '96px',
-        px: '24px',
-      })}
-    >
-      <div className={hstack({ gap: '32px', maxW: '490px', mx: 'auto' })}>
-        <Badge bg='purple.500' status='neutral' kind='solid'>
-          Badge
-        </Badge>
-        <Badge status='info' kind='solid'>
-          Badge
-        </Badge>
-        <Badge status='success'>Badge</Badge>
-        <Badge status='error'>Badge</Badge>
-      </div>
-    </div>
+    <Stack minH='100dvh' pt='14' px='6'>
+      <Alert
+        bg='orange.200'
+        status='info'
+        kind='subtle'
+        title='Title'
+        description='This is a description'
+      />
+    </Stack>
   );
 }
 
